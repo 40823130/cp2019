924c87f412fa472a5271ffd4a3be020c7bb8dd3c
40823130 <40823130@gm.nfu.edu.tw>
2019-11-07 20:38:06 +0800

    add blog

--- publishconf.py
+++ publishconf.py
@@ -30,7 +30,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "40823130cp2019"
+DISQUS_SITENAME = "https-40823130-github-io-cp2019"
 DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 
